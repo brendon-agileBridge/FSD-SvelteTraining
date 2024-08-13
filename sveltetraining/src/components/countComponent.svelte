4468243e63bb486a5a2f23ee7308af4094259644
Brendon VDM <brendonvdm@agilebridge.co.za>
2024-08-13 12:10:48 +0200

    Passing props to a component

--- sveltetraining/src/components/countComponent.svelte
+++ sveltetraining/src/components/countComponent.svelte
@@ -1,8 +1,18 @@
+<script>
+    import {onMount} from "svelte";
+
+    export let initCount = 0;
+    let count = 0;
+
+    onMount(() => {
+        count = initCount;
+    });
+</script>
+
+<br>
 <label>Count is: {count}</label>
 <br>
 <input type="number" bind:value={count}/>
+<br>
 <button on:click={() => {count = 0}}>Reset</button>
-
-<script>
-    let count = 0;
-</script>
+<br>
--- sveltetraining/src/routes/+page.svelte
+++ sveltetraining/src/routes/+page.svelte
@@ -1,10 +1,15 @@
 <script>
     import CountComponent from "../components/countComponent.svelte";
 
+    let tenItems = [1,2,3,4,5,6,7,8,9,10];
+
+
 </script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+{#each tenItems as i}
+    <CountComponent bind:initCount={i} on:countChanged={(val) => alert(JSON.stringify(val))} />
+{/each}
 
-<CountComponent  />
 
