f655760bcf4df271f4ef671f5b40e99d43a449b2
Brendon VDM <brendonvdm@agilebridge.co.za>
2024-08-13 12:15:47 +0200

    Computed functions

--- sveltetraining/src/components/countComponent.svelte
+++ sveltetraining/src/components/countComponent.svelte
@@ -16,10 +16,26 @@
     function reset() {
         count = 0;
     }
+
+    $: letter = function(){
+        let ordA = 'a'.charCodeAt(0);
+        let ordZ = 'z'.charCodeAt(0);
+        let len = ordZ - ordA + 1;
+
+        let s = "";
+        let n = count;
+        while(n >= 0) {
+            s = String.fromCharCode(n % len + ordA) + s;
+            n = Math.floor(n / len) - 1;
+        }
+        return s;
+    };
 </script>
 
 <br>
 <label>Count is: {count}</label>
+<br>
+<label>letter is: {letter()}</label>
 <br>
 <input type="number" bind:value={count}/>
 <br>
