182f2b94a0fa27775ffcb8e5ce7d6c053d8e5a7b
Brendon VDM <brendonvdm@agilebridge.co.za>
2024-08-13 11:17:35 +0200

    1 Way Bound Data

--- sveltetraining/src/routes/+page.svelte
+++ sveltetraining/src/routes/+page.svelte
@@ -1,2 +1,11 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<label>Count is: {count}</label>
+<br>
+Alternatively Count is: 
+<span contenteditable="false" bind:textContent={count}></span>
+
+<script>
+    let count = 0;
+</script>
