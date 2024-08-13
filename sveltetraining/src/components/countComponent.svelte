fdedb1b277f11419c77c3451b5f3097b23a7023c
Brendon VDM <brendonvdm@agilebridge.co.za>
2024-08-13 12:06:35 +0200

    Create a component

---
+++ sveltetraining/src/components/countComponent.svelte
@@ -0,0 +1,8 @@
+<label>Count is: {count}</label>
+<br>
+<input type="number" bind:value={count}/>
+<button on:click={() => {count = 0}}>Reset</button>
+
+<script>
+    let count = 0;
+</script>
--- sveltetraining/src/routes/+page.svelte
+++ sveltetraining/src/routes/+page.svelte
@@ -1,11 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<label>Count is: {count}</label>
-<br>
-<input type="number" bind:value={count}/>
-<button on:click={() => {count = 0}}>Reset</button>
-
-<script>
-    let count = 0;
-</script>
