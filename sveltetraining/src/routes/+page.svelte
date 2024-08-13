36977698e0fd47f79e4b194f0c388075a02e680c
Brendon VDM <brendonvdm@agilebridge.co.za>
2024-08-13 12:09:05 +0200

    Use a component

--- sveltetraining/src/routes/+page.svelte
+++ sveltetraining/src/routes/+page.svelte
@@ -1,3 +1,10 @@
+<script>
+    import CountComponent from "../components/countComponent.svelte";
+
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<CountComponent  />
 
